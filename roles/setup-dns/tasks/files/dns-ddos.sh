bcd5f31ffe0b5ea0f79f468895123ce82ed7641c
cozybear-dev <mjvogel99@gmail.com>
2023-06-30 13:07:11 +0200

    bugfix

--- roles/setup-dns/tasks/files/dns-ddos.sh
+++ roles/setup-dns/tasks/files/dns-ddos.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 (( usage = $(top -b -n 1 -d 1 -p $(pgrep -x unbound) | tail -1 | awk '{print $9}' | cut -f1 -d ".") ))
-if (( usage > 35 )); then
+if (( $usage > 35 )); then
   service unbound restart &
 fi
